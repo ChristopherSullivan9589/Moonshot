eba96c4fe30597abd122129f48cb78d8591d91b4
Chris Sullivan <cks85264@usc.edu>
2024-11-12 12:21:49 -0800

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Christopher Sullivan on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
