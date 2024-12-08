328f9a225d7627c14f232cfb0e987216f84406e0
Chris Sullivan <cks85264@usc.edu>
2024-12-07 17:53:25 -0800

    Done

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Christopher Sullivan on 11/12/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
